d0038df50a6e875b079923a8b89b4d190434193a
BettinaBalind <balindbetti96@gmail.com>
2018-03-02 11:03:57 +0100

    Got a factorial method

--- src/string_operations.c
+++ src/string_operations.c
@@ -9,13 +9,24 @@ size_t my_strlen(char *s)
 	}
 	return (p-s);
 }
+int factorial(int n){
+	int i;
+	int temp=1;
+	for(i=n; i>0; i--){
+		temp*=i;
+	}
+	return temp;
+}
 
 int main(void)
 {
 	int i;
+	int n;	
 	char *s[] = {"Git tutorials","Tutorials Point"};
 	for(i=0;i<2;++i){
 		printf("string length os %s=%d\n", s[i], my_strlen(s[i]));
 	}
+	scanf("%d", &n);
+	factorial(n);
 	return 0;
 }
